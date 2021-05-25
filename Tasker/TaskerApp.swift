5b55e0c9dddb5d61ff4b1f39f2d6c06fa3493b6a
Felix <felix.tesche@gmail.com>
2021-05-25 09:48:55 +0200

    Initial Commit

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Felix Tesche on 25.05.21.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
